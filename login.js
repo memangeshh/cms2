2d3943381047fa12a218225e767b52e76641c149
memangeshh <97848958+memangeshh@users.noreply.github.com>
2022-05-05 22:34:50 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,30 +1,34 @@
 function login(){
 
-    let user_input_email = document.getElementById("email").value;
-    let user_input_password = document.getElementById("password").value;
-
-    
-
-    let local_data = localStorage.getItem("amit");
-    let data = JSON.parse(local_data);
-    console.log(data)
+    let user_input_email = document.getElementById("email").value.trim();
+    let user_input_password = document.getElementById("password").value.trim();
 
     if (user_input_email === ""){
-        console.log("incorrect email")
-        document.getElementById("logHome").innerHTML = "incorrect email";
+        console.log("incorrect email");
+        document.getElementById("logHome").innerHTML = "empty email";
 
     }
-    else if (user_input_email === data.email && user_input_password === data.password){
-        window.open = "index.html"
-        document.getElementById("logHome").innerHTML = "Welcome " + data.firstName +" " + data.lastName;
-        console.log("login success")
-    
-       
-    }else{
-        document.getElementById("logHome").innerHTML = "login failed";
-        console.log("login failed")
+    else if (user_input_password === ""){
+        document.getElementById("logHome").innerHTML = "empty password";
+
+    }
+    else {
+        let local_data = localStorage.getItem(user_input_email);
+        let data = JSON.parse(local_data);
+        console.log(data);
+        if (user_input_email === data.email && user_input_password === data.password){
+           
+            document.getElementById("logHome").innerHTML = "Welcome " + data.firstName +" " + data.lastName;
+            console.log("login success");
+            location.href = "index.html";
+           
+        }else{
+            document.getElementById("logHome").innerHTML = "login failed";
+            console.log("login failed");
+        }
     }
     
-
+document.getElementById("logHome").innerHTML = "invalid data";
+    
    
 }
